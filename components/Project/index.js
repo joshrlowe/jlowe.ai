5ebaa7d91369cb41c5161160da745cd6242154c3
Josh Lowe <joshlowe.cs@gmail.com>
2026-01-21 10:50:08 -0500

    feat: add barrel export for Project components
    Add index.js to components/Project for cleaner imports.

---
+++ components/Project/index.js
@@ -0,0 +1,18 @@
+/**
+ * Project Components barrel export
+ */
+
+export { default as Project } from "./Project";
+export { default as ProjectCard } from "./ProjectCard";
+export { default as ProjectDescription } from "./ProjectDescription";
+export { default as ProjectDetail } from "./ProjectDetail";
+export { default as ProjectFilters } from "./ProjectFilters";
+export { default as ProjectHeader } from "./ProjectHeader";
+export { default as ProjectsEmptyState } from "./ProjectsEmptyState";
+export { default as ProjectSkeleton } from "./ProjectSkeleton";
+export { default as ProjectTeam } from "./ProjectTeam";
+export { default as ProjectTechStack } from "./ProjectTechStack";
+export { default as ProjectTimeline } from "./ProjectTimeline";
+export { default as StatusBadge } from "./StatusBadge";
+
+
